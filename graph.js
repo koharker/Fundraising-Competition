f83cf97bc5d7196199195765757d3b59f6b8ea7f
koharker <koharker@gmail.com>
2019-02-25 12:33:27 -0800

    Update graph.js

--- graph.js
+++ graph.js
@@ -1,7 +1,7 @@
 document('.vertical .progress-fill span').each(function(){
     var percent = $(this).html();
     var pTop = 100 - ( percent.slice(0, percent.length - 1) ) + "%";
-    document(this).parent().css({
+    $(this).parent().css({
       'height' : percent,
       'top' : pTop
     });
