6cb2e8dccc640c5e553762588efca19c61b51c8e
koharker <koharker@gmail.com>
2019-02-25 12:00:41 -0800

    Update graph.js

--- graph.js
+++ graph.js
@@ -1,8 +1,3 @@
-$('.horizontal .progress-fill span').each(function(){
-  var percent = $(this).html();
-  $(this).parent().css('width', percent);
-});
-
 
 $('.vertical .progress-fill span').each(function(){
   var percent = $(this).html();
