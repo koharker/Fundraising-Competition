cd4008ba45a5dd2c3d059abc5fd47b02d27c27f5
koharker <koharker@gmail.com>
2019-02-25 15:06:35 -0800

    Update graph.js

--- graph.js
+++ graph.js
@@ -1,14 +1,16 @@
-$('.horizontal .progress-fill span').each(function(){
-  var percent = $(this).html();
-  $(this).parent().css('width', percent);
-});
+$(document).ready(function(){
+  $('.horizontal .progress-fill span').each(function(){
+    var percent = $(this).html();
+    $(this).parent().css('width', percent);
+  });
 
 
-$('.vertical .progress-fill span').each(function(){
-  var percent = $(this).html();
-  var pTop = 100 - ( percent.slice(0, percent.length - 1) ) + "%";
-  $(this).parent().css({
-    'height' : percent,
-    'top' : pTop
+  $('.vertical .progress-fill span').each(function(){
+    var percent = $(this).html();
+    var pTop = 100 - ( percent.slice(0, percent.length - 1) ) + "%";
+    $(this).parent().css({
+      'height' : percent,
+      'top' : pTop
+    });
   });
 });
